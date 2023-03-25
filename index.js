5930939764dac9ea5e06ca6e469dc2083b1393a4
calvin-arieri <calvin.arieri@student.moringaschoool.com>
2023-03-25 21:34:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.getElementById("main");
+
+ //Removing element from HTML
+main.remove();
+
+const newHeader =document.getElementById("victory");
+
+newHeader.innerHTML = "YOUR-NAME is the champion"
